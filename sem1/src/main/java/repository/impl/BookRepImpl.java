bf1488d0e1ff19125af1a3d7b0db33ae3cb77977
Melnikov Almaz <melnikovalmaz@gmail.com>
2016-05-13 17:03:13 +0300

    Add around aspect

--- sem1/src/main/java/repository/impl/BookRepImpl.java
+++ sem1/src/main/java/repository/impl/BookRepImpl.java
@@ -9,6 +9,7 @@ import utils.DbDriverSingleton;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,29 +21,24 @@ public class BookRepImpl {
     @Autowired
     CityRepository cityRepository;
 
-    public List<Book> findAllWhereNameStartsWithByPassengerId(String name, int id){
+    public List<Book> findAllWhereNameStartsWithByPassengerId(String name, int id) throws SQLException, ClassNotFoundException {
         List<Book> data = new ArrayList<>();
-        try {
-            Connection dbConnection = DbDriverSingleton.getDbConnection();
-            PreparedStatement stmt = dbConnection.prepareStatement("select * from book as b where b.name like ? and b.passenger = ?");
-            stmt.setString(1, name + "%");
-            stmt.setInt(2, id);
-            ResultSet rs = stmt.executeQuery();
-            while (rs.next()) {
-                Book book = new Book();
-                book.setId(rs.getInt("id"));
-                book.setCity(cityRepository.findOneById(rs.getInt("city")));
-                book.setCost(rs.getInt("cost"));
-                book.setFromstreet(rs.getString("fromstreet"));
-                book.setFromhouse(rs.getString("fromhouse"));
-                book.setTostreet(rs.getString("tostreet"));
-                book.setTohouse(rs.getString("tohouse"));
-                book.setName(rs.getString("name"));
-                data.add(book);
-            }
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.err.println(e.getClass().getName() + ": " + e.getMessage());
+        Connection dbConnection = DbDriverSingleton.getDbConnection();
+        PreparedStatement stmt = dbConnection.prepareStatement("select * from book as b where b.name like ? and b.passenger = ?");
+        stmt.setString(1, name + "%");
+        stmt.setInt(2, id);
+        ResultSet rs = stmt.executeQuery();
+        while (rs.next()) {
+            Book book = new Book();
+            book.setId(rs.getInt("id"));
+            book.setCity(cityRepository.findOneById(rs.getInt("city")));
+            book.setCost(rs.getInt("cost"));
+            book.setFromstreet(rs.getString("fromstreet"));
+            book.setFromhouse(rs.getString("fromhouse"));
+            book.setTostreet(rs.getString("tostreet"));
+            book.setTohouse(rs.getString("tohouse"));
+            book.setName(rs.getString("name"));
+            data.add(book);
         }
         return data;
     }
