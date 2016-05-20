ef61716e12df775f88a2942a64bf8afc77e0e821
Melnikov Almaz <melnikovalmaz@gmail.com>
2016-05-20 13:19:14 +0300

    Define require objects

---
+++ sem2_game/src/sample/Int.java
@@ -0,0 +1,20 @@
+package sample;
+
+/**
+ * Created by Almaz on 15.05.2016.
+ */
+public class Int {
+    private int value;
+
+    public Int(int value) {
+        this.value = value;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
--- sem2_game/src/sample/Main.java
+++ sem2_game/src/sample/Main.java
@@ -2,22 +2,44 @@ package sample;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
+import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
+import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
-public class Main extends Application {
+import java.io.IOException;
+import java.util.ArrayList;
 
-    @Override
-    public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+// Collect the Money Bags!
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
+        launch(args);
     }
 
+    @Override
+    public void start(Stage theStage) throws IOException {
+        theStage.setTitle("Be free!");
 
-    public static void main(String[] args) {
-        launch(args);
+        Pane root = FXMLLoader.load(getClass().getResource("game.fxml"));
+        Scene theScene = new Scene(root);
+        theStage.setScene(theScene);
+
+        ArrayList<Node> walls = new ArrayList<>();
+        walls.addAll(theScene.getRoot().lookupAll("#"));
+        walls.remove(theScene.getRoot().lookup("#pane"));
+        walls.remove(theScene.getRoot().lookup("#user"));
+        walls.remove(theScene.getRoot().lookup("#finish"));
+        walls.remove(theScene.getRoot().lookup("#small"));
+        walls.remove(theScene.getRoot().lookup("#speed"));
+        walls.remove(theScene.getRoot().lookup("#shape"));
+        walls.remove(theScene.getRoot().lookup("#change"));
+
+        Circle user = (Circle) theScene.getRoot().lookup("#user");
+        Node finish = theScene.getRoot().lookup("#finish");
+        Int speed = new Int(5);
+        theStage.show();
     }
 }
