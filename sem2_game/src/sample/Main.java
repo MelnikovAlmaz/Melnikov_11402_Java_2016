76a589d1da80b8e7efb04d1e2f6d1880d4136815
Melnikov Almaz <melnikovalmaz@gmail.com>
2016-05-20 13:22:12 +0300

    Add game end

--- sem2_game/src/sample/Main.java
+++ sem2_game/src/sample/Main.java
@@ -1,10 +1,13 @@
 package sample;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Node;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
@@ -14,8 +17,10 @@ import java.io.IOException;
 import java.util.ArrayList;
 
 // Collect the Money Bags!
-public class Main extends Application {
-    public static void main(String[] args) {
+public class Main extends Application
+{
+    public static void main(String[] args)
+    {
         launch(args);
     }
 
@@ -44,11 +49,11 @@ public class Main extends Application {
             @Override
             public void handle(KeyEvent event) {
                 for (int i = 0; i < speed.getValue(); i++) {
-                    switch (event.getCode()) {
+                    switch (event.getCode()){
                         case W:
                             user.setLayoutY(user.getLayoutY() - 1);
-                            for (Node wall : walls) {
-                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())) {
+                            for (Node wall: walls) {
+                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())){
                                     user.setLayoutY(user.getLayoutY() + 1);
                                     break;
                                 }
@@ -56,8 +61,8 @@ public class Main extends Application {
                             break;
                         case S:
                             user.setLayoutY(user.getLayoutY() + 1);
-                            for (Node wall : walls) {
-                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())) {
+                            for (Node wall: walls) {
+                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())){
                                     user.setLayoutY(user.getLayoutY() - 1);
                                     break;
                                 }
@@ -65,8 +70,8 @@ public class Main extends Application {
                             break;
                         case A:
                             user.setLayoutX(user.getLayoutX() - 1);
-                            for (Node wall : walls) {
-                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())) {
+                            for (Node wall: walls) {
+                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())){
                                     user.setLayoutX(user.getLayoutX() + 1);
                                     break;
                                 }
@@ -74,13 +79,30 @@ public class Main extends Application {
                             break;
                         case D:
                             user.setLayoutX(user.getLayoutX() + 1);
-                            for (Node wall : walls) {
-                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())) {
+                            for (Node wall: walls) {
+                                if (user.getBoundsInParent().intersects(wall.getBoundsInParent())){
                                     user.setLayoutX(user.getLayoutX() - 1);
                                     break;
                                 }
                             }
                             break;
+                    }
+                    if(user.getBoundsInParent().intersects(finish.getBoundsInParent())){
+                        Alert alert = new Alert(Alert.AlertType.CONFIRMATION,"Are you want to play again?");
+                        alert.showAndWait().ifPresent(response -> {
+                            if (response == ButtonType.OK) {
+                                try {
+                                    theScene.setRoot(FXMLLoader.load(getClass().getResource("game.fxml")));
+                                } catch (IOException e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                            if (response == ButtonType.CANCEL){
+                                Platform.exit();
+                                System.exit(0);
+                            }
+                        });
+                        break;
                     }
                 }
             }
