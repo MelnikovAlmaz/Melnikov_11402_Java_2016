a8339c2186c27bb8d41da96592eec4fb6a190bb1
Melnikov Almaz <melnikovalmaz@gmail.com>
2016-05-27 09:06:18 +0300

    Add client api methods

--- sem1/src/main/java/controllers/ApplicationController.java
+++ sem1/src/main/java/controllers/ApplicationController.java
@@ -1,18 +1,17 @@
 package controllers;
 
-import entity.City;
-import entity.Driver;
-import entity.Passenger;
+import entity.*;
+import org.codehaus.jackson.map.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
-import service.CityService;
-import service.DriverService;
-import service.PassengerService;
+import service.*;
 
+import java.io.IOException;
+import java.net.URLDecoder;
 import java.util.List;
 
 /**
@@ -24,9 +23,13 @@ public class ApplicationController {
     @Autowired
     CityService cityService;
     @Autowired
+    BookService bookService;
+    @Autowired
     PassengerService passengerService;
     @Autowired
     DriverService driverService;
+    @Autowired
+    FeedBackService feedBackService;
 
     @RequestMapping(value = "/authorization/passenger", method = RequestMethod.GET)
     @ResponseBody
@@ -46,10 +49,41 @@ public class ApplicationController {
         return driver;
     }
 
-    @RequestMapping(value = "/citys", method = RequestMethod.GET)
+    @RequestMapping(value = "/city", method = RequestMethod.GET)
     @ResponseBody
     public List<City> listCity() {
         List<City> cities = cityService.getAllCities();
         return cities;
+    }
+
+    @RequestMapping(value = "/book", method = RequestMethod.GET)
+    @ResponseBody
+    public List<Book> listBook(@RequestParam(value = "passenger_id") int passenger_id) {
+        List<Book> books = bookService.getAllBooksByPassengerId(passenger_id);
+        return books;
+    }
+
+    @RequestMapping(value = "/bookAdd", method = RequestMethod.GET)
+    @ResponseBody
+    public void createBook(@RequestParam(value = "json") String bookJSON) throws IOException {
+        String json = URLDecoder.decode(bookJSON);
+        Book book = new ObjectMapper().readValue(json, Book.class);
+        bookService.addNewBook(book);
+    }
+
+    @RequestMapping(value = "/passengerUpdate", method = RequestMethod.GET)
+    @ResponseBody
+    public void updatePassenger(@RequestParam(value = "json") String passengerJSON) throws IOException {
+        String json = URLDecoder.decode(passengerJSON);
+        Passenger passenger = new ObjectMapper().readValue(json, Passenger.class);
+        passengerService.update(passenger);
+    }
+
+    @RequestMapping(value = "/feedbackAdd", method = RequestMethod.GET)
+    @ResponseBody
+    public void createFeedBack(@RequestParam(value = "json") String feedbackJSON) throws IOException {
+        String json = URLDecoder.decode(feedbackJSON);
+        Feedback feedback = new ObjectMapper().readValue(json, Feedback.class);
+        feedBackService.addNewFeedBack(feedback);
     }
 }
