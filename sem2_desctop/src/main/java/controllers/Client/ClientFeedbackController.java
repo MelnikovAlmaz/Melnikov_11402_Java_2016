406210298504f33c4ebb196c5f18ecb1b6a0232a
Melnikov Almaz <melnikovalmaz@gmail.com>
2016-05-28 16:24:09 +0300

    Rename client controllers

---
+++ sem2_desctop/src/main/java/controllers/Client/ClientFeedbackController.java
@@ -0,0 +1,63 @@
+package controllers.Client;
+
+import entity.City;
+import entity.Feedback;
+import entity.Passenger;
+import environment.GameEnvironment;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import models.CityModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import web.ServerApi;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+@Component
+public class ClientFeedbackController extends ClientMenuController{
+    @Autowired
+    GameEnvironment gameEnvironment;
+    @Autowired
+    ServerApi serverApi;
+    @Autowired
+    CityModel cityModel;
+
+    @FXML
+    ChoiceBox city_fld;
+    @FXML
+    TextArea what_happend_ta;
+    @FXML
+    DatePicker date_dp;
+    @FXML
+    TextField phone_fld;
+    @FXML
+    TextArea addition_ta;
+
+    @FXML
+    public void initialize(){
+        Passenger passenger = gameEnvironment.getPassenger();
+        city_fld.setItems(cityModel.getCityNames());
+        city_fld.setValue(passenger.getCity().getName());
+        date_dp.setValue(LocalDate.now());
+        phone_fld.setText(passenger.getPhone());
+    }
+
+    @FXML
+    public void sendFeedBack(){
+        Feedback feedback = new Feedback();
+        City city = new City();
+        city.setName((String) city_fld.getValue());
+        city.setId(city_fld.getItems().indexOf(city_fld.getValue())+1);
+        feedback.setCity(city);
+        feedback.setPhone(phone_fld.getText());
+        feedback.setAdditional(addition_ta.getText());
+        feedback.setSubject(what_happend_ta.getText());
+        feedback.setDate(Date.valueOf(date_dp.getValue()));
+
+        serverApi.createFeedBack(feedback);
+    }
+}
---
+++ sem2_desctop/src/main/java/controllers/Client/ClientNewOrderController.java
@@ -0,0 +1,73 @@
+package controllers.Client;
+
+import entity.Book;
+import entity.City;
+import entity.Passenger;
+import environment.GameEnvironment;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import models.CityModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import web.ServerApi;
+
+@Component
+public class ClientNewOrderController extends ClientMenuController {
+    @Autowired
+    private GameEnvironment gameEnvironment;
+    @Autowired
+    private ServerApi serverApi;
+    @Autowired
+    private CityModel cityModel;
+
+    @FXML
+    TextField name_fld;
+    @FXML
+    ChoiceBox city_fld;
+    @FXML
+    TextField from_street_fld;
+    @FXML
+    TextField from_house_fld;
+    @FXML
+    TextField from_flat_fld;
+    @FXML
+    TextField from_place_fld;
+    @FXML
+    TextField to_street_fld;
+    @FXML
+    TextField to_house_fld;
+    @FXML
+    TextField phone_fld;
+
+    @FXML
+    public void initialize(){
+        Passenger passenger = gameEnvironment.getPassenger();
+        from_street_fld.setText(passenger.getStreet());
+        from_house_fld.setText(passenger.getHouse());
+        phone_fld.setText(passenger.getPhone());
+        city_fld.setItems(cityModel.getCityNames());
+        city_fld.setValue(passenger.getCity().getName());
+    }
+    @FXML
+    public void sendNewOrder(){
+        Book book = new Book();
+        book.setName(name_fld.getText());
+        City city = new City();
+        city.setName((String) city_fld.getValue());
+        city.setId(city_fld.getItems().indexOf(city_fld.getValue())+1);
+        book.setCity(city);
+        book.setFromstreet(from_street_fld.getText());
+        book.setFromhouse(from_house_fld.getText());
+        book.setTostreet(to_street_fld.getText());
+        book.setTohouse(to_house_fld.getText());
+        book.setPhone(phone_fld.getText());
+        book.setFromblock(Integer.parseInt(from_flat_fld.getText()));
+        book.setFromplace(from_place_fld.getText());
+        book.setPassenger(gameEnvironment.getPassenger());
+        book.setCost(0);
+        book.setState(0);
+
+        serverApi.createBook(book);
+    }
+}
